10e449aa49f03f98919bc79eabcc0a3c9a7e1584
MrPOS666 <ruitaozou@gmail.com>
2024-09-24 17:08:58 -0400

    Finished JSONTranslator

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -16,7 +16,8 @@ import org.json.JSONObject;
  * An implementation of the Translator interface which reads in the translation
  * data from a JSON file. The data is read in once each time an instance of this class is constructed.
  */
-public class JSONTranslator implements Translator {
+public class
+JSONTranslator implements Translator {
 
     private Map<String, Map<String, Object>> countriesTranslation = new HashMap<>();
 
